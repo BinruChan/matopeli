f9aae77e86fdc8285a1259470f87f94a4906d5aa
Binru <44642708+BinruChan@users.noreply.github.com>
2020-08-08 15:16:02 +0300

    Fix scam

--- scoreboard.php
+++ scoreboard.php
@@ -24,31 +24,8 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 	case "GET":
 		
 		// Query the top 4 scores from the database
-		$result = mysqli_query($dblink, "SELECT name, score FROM ".$table." ORDER by score DESC LIMIT 4");
-		
-		// Process result into an array
-		$i = 1;
-		$top5[0] = array("name"=>"Ossi Portaankorva");
-		while ($row = mysqli_fetch_assoc($result)) {
-			
-			$top5[$i] = $row;
-			$i = $i + 1;
-		}
-		
-		
-		// Add "Ossi Portaankorva"
-		if ($top5[1]["score"] < 50) {
-			
-			$top5[0] = array(
-				"name" => "Ossi Portaankorva",
-				"score" => "54"
-			);
-		} else {
-			
-			$top5[0] = array(
-				"name" => "Ossi Portaankorva",
-				"score" => strval(ceil($top5[1]["score"]/9 + 1)*9)
-			);
+		$result = mysqli_query($dblink, "SELECT name, score FROM ".$table." ORDER by score DESC LIMIT 5");
+	
 		}
 		
 		header("Content-Type: application/json; charset: utf-8");
